9974a3ae2bcb6f522e934bba07e1dc5ef652b260
Mathew Webb <matsofargone@gmail.com>
2023-04-28 21:15:52 -0700

    added showing cards with displaybooks function

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ let myLibrary = [];
 const addButton = document.getElementById('addButton');
 const closeFormButton = document.getElementById('closeForm');
 const addBookButton = document.getElementById('addBook');
-// const defaultForm = document.getElementById('myForm');
 const form = document.querySelector('form');
 
- 
+ //submit button actions
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     addBookToLibrary();
     closeForm();
+    displayBooks();
+    showAddbutton();
+   
+    
 })
 
 
@@ -35,38 +38,88 @@ function Book(title, author, pages, read){
     this.pages = pages
     this.read = read
     
-    this.info = function(){
-        return (`${title} by ${author}, ${pages} pages, ${read}`)
-    }
 
 }
 
 function addBookToLibrary(){
-    //code here
       // takes user input 
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const pages = document.querySelector('#pages').value;
     const read = document.querySelector('#read').value;
     //create object
-    const entry = Object.create(Book);
-    entry.title = title;
-    entry.author = author;
-    entry.pages = pages;
-    entry.read = read;
-   
+    const entry = new Book(title, author, pages, read);
     
+   
     //store users input into an array 
     myLibrary.push(entry);
-
-    console.log(myLibrary);
+    
+   
+    //clears form after submission 
+   form.reset();
+   
 
 }
 
 function displayBooks(){
-    //code here 
-    myLibrary.forEach((book) =>{
+    
+    const cardlist = document.querySelector('#cards');
+    cardlist.innerHTML = '';
 
+    if (!cardlist) {
+        console.error('Cannot find card list');
+        return;
+    }
+
+    myLibrary.forEach((book, index) =>{
+
+      
+        console.log('title:', book.title);
+        console.log('author:', book.author);
+        console.log('pages:', book.pages);
+        console.log('checkbox', book.checkbox);
+        
+        
+        const card =  document.createElement('div');
+        card.classList.add('card');
+        
+        card.dataset.index = index;
+        
+        console.log(index);
+
+        const title = document.createElement('h4');
+        title.textContent = book.title;
+
+        const author = document.createElement('h5');
+        author.textContent = book.author;
+
+        const pages = document.createElement('p');
+        pages.textContent = book.pages;
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = book.read;
+        checkbox.addEventListener('change', () => {
+            book.read = checkbox.checked;
+          });
+
+        const remove = document.createElement('input');
+        remove.type = 'button';
+        remove.onclick = myLibrary.pop(book);
+
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(checkbox);
+        card.appendChild(remove);
+        
+          
+        
+
+       
+        
+       cardlist.appendChild(card);
+       
     });
 }
 
